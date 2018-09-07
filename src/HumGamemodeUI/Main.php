5f298d78a8bc69796369eb4326aac459a71db171
BlackWebPES <42685273+BlackWebPES@users.noreply.github.com>
2018-09-07 06:22:03 -0400

    Update Main.php

--- src/HumGamemodeUI/Main.php
+++ src/HumGamemodeUI/Main.php
@@ -12,7 +12,7 @@ use jojoe77777\FormAPI;
 Class Main extends PluginBase{
   
   public function onEnable(){
-    $this->getLogger()->info("[§eGamemode§bUI§f] §aEnable");
+    $this->getLogger()->info("gamemode ui enabled");
   }
   
   public function onCommand(CommandSender $sender, Command $command, String $label, array $args) : bool {
@@ -24,38 +24,36 @@ Class Main extends PluginBase{
           if( !is_null($data)) {
             switch($result) {
             case 0:
-              $sender->sendMessage("Cretive mode §aChange gamemode to gm 1");
-              $sender->addTitle("§eCreative mode", "§fCreative mode is enable");
+              $sender->sendMessage("cretive mode change gamemode to gm 1");
+              $sender->addTitle("creative mode", "creative mode is enabled");
               $sender->setGamemode(1);
               break;
             case 1:
-              $sender->sendMessage("Survival mode §aChange gamemode to gm 0");
-              $sender->addTitle("§eSurvival mode", "§fSurvival mode is enable");
+              $sender->sendMessage("survival mode change gamemode to gm 0");
+              $sender->addTitle("survival mode", "survival mode is enabled");
               $sender->setGamemode(0);
               break;
             case 2:
-              $sender->sendMessage("Adventure mode §aChange gamemode to gm 2");
-              $sender->addTitle("§eAdventure mode", "§fAdventure mode is enable");
+              $sender->sendMessage("adventure mode change gamemode to gm 2");
+              $sender->addTitle("adventure mode", "adventure mode is enabled");
               $sender->setGamemode(2);
               break;
             case 3:
-              $sender->sendMessage("Spector mode §aChange gamemode to gm 3");
-              $sender->addTitle("§eSpector mode", "§fSpector mode is enable");
+              $sender->sendMessage("spector mode change gamemode to gm 3");
+              $sender->addTitle("spector mode", "spector mode is enabled");
               $sender->setGamemode(3);
               default:
                 return;
                 }
            }
           });
-          $form->setTitle("§7§lGamemodeUI");
+          $form->setTitle("§c§lBlackWebPE Gamemode-UI");
           $form->addDropdown("Gamemode", ["Creative", "Survival", "Adventure", "Spector"]);
           $form->sendToPlayer($sender);
       } else {
-        $sender->sendMessage("§cYou did not operater or you don't have permission!");
+        $sender->sendMessage("Your Not Staff");
       }
     }
     return true;
   }
 }
-
- 
